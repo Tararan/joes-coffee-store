3f5b048405a1e810c1bea8478556d69cc8053aac
Tararan <30859637+Tararan@users.noreply.github.com>
2018-11-27 22:06:14 +0100

    added build folder

---
+++ build/precache-manifest.9acc88532039ec9a790e351ca3d809fb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "5acea0425cc72124aa9b315a30b89a4f",
+    "url": "/static/media/logo-large.5acea042.png"
+  },
+  {
+    "revision": "85c9baca95f63cdc2cf2",
+    "url": "/static/css/main.168005ba.chunk.css"
+  },
+  {
+    "revision": "fe7070e077eed0e9029f",
+    "url": "/static/js/1.fe7070e0.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "90dae8f89887090412efad40031e99a5",
+    "url": "/static/media/header-3.90dae8f8.jpg"
+  },
+  {
+    "revision": "85c9baca95f63cdc2cf2",
+    "url": "/static/js/main.85c9baca.chunk.js"
+  },
+  {
+    "revision": "00dd2fe491b7cdd39c64a0a88c08a08c",
+    "url": "/static/media/down-arrow-sketch.00dd2fe4.png"
+  },
+  {
+    "revision": "1acd67df2b4d8712daabf20fac74ed19",
+    "url": "/static/media/coffeeheader.1acd67df.jpg"
+  },
+  {
+    "revision": "7346806861a7e8444d01ec7b54222e9e",
+    "url": "/static/media/black-coffee.73468068.png"
+  },
+  {
+    "revision": "f2f1303552836da00492f859f55499b1",
+    "url": "/static/media/bestseller-gold.f2f13035.png"
+  },
+  {
+    "revision": "917e5f8fa4df07446eeb3ec372aadf1c",
+    "url": "/index.html"
+  }
+];
